fdf8f6476d9328d57190deef1d57f90ae006c23f
MarcosPimienta <fenix3819@gmail.com>
2025-02-21 00:11:46 -0500

    fix: Styles and disable certain testing features by commenting out

--- src/components/GameBoard/WinnerModal.tsx
+++ src/components/GameBoard/WinnerModal.tsx
@@ -8,7 +8,7 @@ interface WinnerModalProps {
 
 const WinnerModal: React.FC<WinnerModalProps> = ({ winner }) => {
   return (
-      <div className="winner-message">
+      <div className="winner-message dna-demo" data-augmented-ui="tl-clip tr-clip br-2-clip-y bl-2-clip-y border">
         <h2>Congratulations!</h2>
         <p>Player {winner.name} has won the game!</p>
       </div>
